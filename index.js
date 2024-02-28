5044111a640af854ac178920a690d813c2e68de8
Abdosadani <Abdosadani8@gmail.com>
2024-02-28 16:18:28 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const App = express();
+
+App.set("port",(process.env.port || 8000));
+
+App.use(bodyParser.urlencoded( { extended: false } ));
+
+App.use(bodyParser.json());
+
+App.get("/", function(req,res){
+    res.send("Hi! I'm just a chatbot web app");
+})
+
+App.listen(App.get("port"),function(){
+    console.log("Server is running and listening on port " + App.get("port"));
+})
